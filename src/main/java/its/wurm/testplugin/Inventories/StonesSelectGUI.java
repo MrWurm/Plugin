15e0875a133407f223a6a77eddb37f85a88a2cec
MrWurm <wurmywurmanonymous@gmail.com>
2022-01-17 17:42:38 +0100

    Added a lot of stuff (agro system, shield system, item save for custom item system, more stats, reforge stones, main menu gui, mob drop system, saved 4k lines of code with better skill xp function, more mobs, treasure from excavating, working fortune (for farming, mining, and excavating), better system to load/update an item's lore, made some ability functions more compact, a lot of new items, changed magic scaling, added rank command, changed mob types)

---
+++ src/main/java/its/wurm/testplugin/Inventories/StonesSelectGUI.java
@@ -0,0 +1,57 @@
+package its.wurm.testplugin.Inventories;
+
+import its.wurm.testplugin.Items.Items;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.Plugin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StonesGUI implements InventoryHolder {
+
+    private Inventory main;
+
+    static Plugin plugin;
+
+    public StonesGUI(Plugin plugin) {
+        this.plugin = plugin;
+        main = Bukkit.createInventory(this, 54, "Reforge Stones - 1");
+        init();
+    }
+
+    private void init()
+    {
+        ItemStack item;
+        List<String> lore = new ArrayList<>();
+
+        //Adding in all instances of the reforge stones
+        main.setItem(0, Items.WHETSTONE.getItem(plugin));
+
+        //Bottom Row with options to change pages and a go back button
+        for (int i = 45; i < 54; i++) {
+            main.setItem(i, Items.MENU_GLASS.getItem(plugin));
+        }
+        main.setItem(49, Items.BACK_ARROW.getItem(plugin));
+
+    }
+
+    private ItemStack createItem(String name, Material mat, List<String> lore) {
+        ItemStack item = new ItemStack(mat, 1);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(name);
+        meta.setLore(lore);
+        item.setItemMeta(meta);
+        return item;
+
+    }
+
+    @Override
+    public Inventory getInventory() {
+        return main;
+    }
+}
